b72b46f04551b068950e830a078a9adc842be0c9
Vikas <102462411+Vikas-Sahani@users.noreply.github.com>
2022-08-08 18:36:02 +0530

    changed a code for delete btn

--- src/components/App.js
+++ src/components/App.js
@@ -1,85 +1,82 @@
 import React, { useState } from "react";
 import "./../styles/App.css";
 
-let GlobalId =1;
+// let GlobalId =1;
 function App() {
 	const [inputVal, setInputVal] = useState("");
 	const [data, setData] = useState([]);
+	const [cnt, setCnt] = useState(1);
 
 	return (
 	<div id="main">
 		{/* //Do not alter main div
 		//Please do not alter the functional component as tests depend on the type of component. */}
-		{/* <form onSubmit={function(event){
-			event.preventDefault();
+		<input 
+			type='text' id="task"
+			onChange={function(e){
+				setInputVal(e.target.value);
+			}}
+			value={inputVal}
+		/> &nbsp;
+		<button 
+			type="submit" 
+			id="btn" 
+			onClick={function(){
+				if(inputVal.length===0){
+					return;
+				}
 
-			if(inputVal.length===0){
-				return;
-			}
+				// let copyOfData = data.slice();
+				// let taskObj = {
+				// 	task:inputVal,
+				// 	id:GlobalId++,
+				// 	complete:false
+				// }
+				// copyOfData.push(taskObj);
+				setData([...data, inputVal]);
 
-			let copyOfData= data.slice();
-			let taskObj ={
-				task:inputVal,
-				id:GlobalId++,
-				complete:false
-			}
-			copyOfData.push(taskObj);
-			setData(copyOfData);
-
-			setInputVal("")
-		}}> */}
-			<input type='text' id="task"
-				 onChange={function(e){
-					setInputVal(e.target.value);
-				}}
-				value={inputVal}/> &nbsp;
-			<button type="submit" id="btn" onClick={function(event){
-					// event.preventDefault();
-
-					if(inputVal.length===0){
-						return;
-					}
-
-					let copyOfData= data.slice();
-					let taskObj ={
-						task:inputVal,
-						id:GlobalId++,
-						complete:false
-					}
-					copyOfData.push(taskObj);
-					setData(copyOfData);
-
-					setInputVal("")
-		}}>Add</button>
-		{/* </form>	 */}
+				setInputVal("")
+			}}
+		>Add</button>
 
 		<ol>
-			{data.map((obj)=>{
-				let task = obj.task;
-				let id = obj.id;
-				let complete= obj.complete;
+			{data.map((obj, idx)=>{
+				// let task = obj.task;
+				// let id = obj.id;
+				// let complete= obj.complete;
 
 				return(<>
-					<li key={id} className="list">
-						<input type="text" disabled={!complete}
-							onChange={function(e){
-								let copyOfData = data.slice();
-								for(let taskObj of copyOfData){
-									if(taskObj.id === id){
-										taskObj.task = e.target.value;
-									}
-								}
+					<li 
+						// key={id} 
+						className="list"
+					>
+						<input 
+							type="text" 
+							// disabled={!complete}
+							// onChange={function(e){
+							// 	let copyOfData = data.slice();
+							// 	for(let taskObj of copyOfData){
+							// 		if(taskObj.id === id){
+							// 			taskObj.task = e.target.value;
+							// 		}
+							// 	}
+							// 	setData(copyOfData);
+							// }} 
+							// value={task}
+							value={obj}
+						/> &nbsp;
+						<button 
+							className="delete" 
+							onClick={function(e){
+								let copyOfData = data.filter((obj, ElmentIdx)=>{
+									// return obj.id !== id;
+									return idx !== ElmentIdx;
+								})
 								setData(copyOfData);
-							}} value={task}
-							/> &nbsp;
-						<button className="delete" onClick={function(){
-							let copyOfData = data.filter((obj)=>{
-								return obj.id !== id;
-							})
-							setData(copyOfData);
 						}}>Delete</button> &nbsp;
-						{complete?
-						<button className="editTask saveTask" 
+						{/* {complete?
+						<button 
+							className="editTask saveTask" 
 							disabled={task.length===0}
 							onClick={function(e){
 								console.log(e.target);
@@ -103,7 +100,7 @@ function App() {
 								}
 								setData(copyOfData);
 						}}>Edit</button>
-						} 
+						}  */}
 					</li>
 				</>)
 			})}
