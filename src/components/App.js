55f7cca668a2c846fd3a33a644daf7536092ce98
Vikas <102462411+Vikas-Sahani@users.noreply.github.com>
2022-10-24 16:14:28 +0530

    uploading ans from newBranch

--- src/components/App.js
+++ src/components/App.js
@@ -1,111 +1,125 @@
-import React, { useState } from "react";
+import React, { Fragment, useState } from "react";
 import "./../styles/App.css";
 
-// let GlobalId =1;
+let GlobalId = 1;
 function App() {
-	const [inputVal, setInputVal] = useState("");
-	const [data, setData] = useState([]);
-	const [cnt, setCnt] = useState(1);
+  const [inputVal, setInputVal] = useState("");
+  const [data, setData] = useState([]);
 
-	return (
-	<div id="main">
-		{/* //Do not alter main div
-		//Please do not alter the functional component as tests depend on the type of component. */}
-		<input 
-			type='text' id="task"
-			onChange={function(e){
-				setInputVal(e.target.value);
-			}}
-			value={inputVal}
-		/> &nbsp;
-		<button 
-			type="submit" 
-			id="btn" 
-			onClick={function(){
-				if(inputVal.length===0){
-					return;
-				}
+  return (
+    <div id="main">
+      <form>
+        <input
+          type="text"
+          id="task"
+          onChange={function (e) {
+            setInputVal(e.target.value);
+          }}
+          value={inputVal}
+        />{" "}
+        &nbsp;
+        <button
+          type="submit"
+          id="btn"
+          onClick={function (event) {
+            event.preventDefault();
+            if (inputVal.length > 0) {
+              let copyOfData = data.slice();
+              let taskObj = {
+                task: inputVal,
+                id: GlobalId++,
+                complete: false,
+              };
+              copyOfData.push(taskObj);
+              setData(copyOfData);
 
-				// let copyOfData = data.slice();
-				// let taskObj = {
-				// 	task:inputVal,
-				// 	id:GlobalId++,
-				// 	complete:false
-				// }
-				// copyOfData.push(taskObj);
-				setData([...data, inputVal]);
+              setInputVal("");
+            }
+          }}
+        >
+          Add
+        </button>
+      </form>
 
-				setInputVal("")
-			}}
-		>Add</button>
+      <ol>
+        {data.map((obj) => {
+          let task = obj.task;
+          let id = obj.id;
+          let complete = obj.complete;
 
-		<ol>
-			{data.map((obj, idx)=>{
-				// let task = obj.task;
-				// let id = obj.id;
-				// let complete= obj.complete;
+          return (
+            <Fragment key={id}>
+              {!complete ? (
+                <div>
+                  <li className="list">{task}</li>
+                  <button
+                    className="edit"
+                    onClick={function () {
+                      let copyOfData = data.slice();
+                      for (let object of copyOfData) {
+                        if (object.id === id) {
+                          object.complete = !complete;
+                        }
+                      }
+                      setData(copyOfData);
+                    }}
+                  >
+                    Edit
+                  </button>
 
-				return(<>
-					<li 
-						// key={id} 
-						className="list"
-					>
-						<input 
-							type="text" 
-							// disabled={!complete}
-							// onChange={function(e){
-							// 	let copyOfData = data.slice();
-							// 	for(let taskObj of copyOfData){
-							// 		if(taskObj.id === id){
-							// 			taskObj.task = e.target.value;
-							// 		}
-							// 	}
-							// 	setData(copyOfData);
-							// }} 
-							// value={task}
-							value={obj}
-						/> &nbsp;
-						<button 
-							className="delete" 
-							onClick={function(e){
-								let copyOfData = data.filter((obj, ElmentIdx)=>{
-									// return obj.id !== id;
-									return idx !== ElmentIdx;
-								})
-								setData(copyOfData);
-						}}>Delete</button> &nbsp;
-						{/* {complete?
-						<button 
-							className="editTask saveTask" 
-							disabled={task.length===0}
-							onClick={function(e){
-								console.log(e.target);
-								let copyOfData = data.slice();
-								for(let taskEl of copyOfData){
-									if(taskEl.id === id){
-										taskEl.complete = !taskEl.complete;
-									}
-								}
-								setData(copyOfData);
-								setInputVal("");
-							}}
-						>Save</button>:
-						<button className="edit" 
-							onClick={function(){
-								let copyOfData = data.slice();
-								for(let taskEl of copyOfData){
-									if(taskEl.id === id){
-										taskEl.complete = !taskEl.complete;
-									}
-								}
-								setData(copyOfData);
-						}}>Edit</button>
-						}  */}
-					</li>
-				</>)
-			})}
-		</ol>
-	</div>
-	);
+                  <button
+                    className="delete"
+                    onClick={function () {
+                      let newArr = data.filter(function (obj) {
+                        return id !== obj.id;
+                      });
+                      setData(newArr);
+                    }}
+                  >
+                    Delete
+                  </button>
+                </div>
+              ) : (
+                <li>
+                  <input
+                    type="text"
+                    className="editTask"
+                    onChange={function (e) {
+                      if (e.target.value.length > 0) {
+                        let copyOfData = data.slice();
+                        for (let object of copyOfData) {
+                          if (object.id === id) {
+                            object.task = e.target.value;
+                          }
+                        }
+                        setData(copyOfData);
+                      } else {
+                        return;
+                      }
+                    }}
+                  />
+                  <br />
+                  <button
+                    className="saveTask"
+                    onClick={function () {
+                      let copyOfData = data.slice();
+                      for (let object of copyOfData) {
+                        if (object.id === id) {
+                          object.complete = !complete;
+                        }
+                      }
+                      setData(copyOfData);
+                    }}
+                  >
+                    saveTask
+                  </button>
+                </li>
+              )}
+            </Fragment>
+          );
+        })}
+      </ol>
+    </div>
+  );
 }
 export default App;
